a691969383bb0183f1a8181877498e4c342eca2c
puran-git-for-web <pramratan2022@gmail.com>
2026-07-15 02:20:38 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 'use client';
-import TaxFormsWizard from '../../components/form/TaxFormsWizard';
-export default function HomePage(){ return <main className='min-h-screen bg-slate-50 p-8'><TaxFormsWizard /></main>; }
+import TaxFormsWizard from '../components/form/TaxFormsWizard';
+export default function HomePage() { 
+  return <main className='min-h-screen bg-slate-50 p-8'><TaxFormsWizard /></main>; 
+}
